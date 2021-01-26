5a5241b035c39083729d5bdafcfe7b8187857eec
Benis Tambe <will.jenifer@yahoo.com>
2021-01-25 23:47:01 -0500

    commit tree 3ea3dee15e9a69462dc95529c1faaeb1cd55c573

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_accessor :roster, :name, :add_student
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster.has_key?(grade)
+           @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+    def grade(grade)
+        if @roster.has_key?(grade)
+            @roster[grade]
+        else
+            puts "grade doesn't exist"
+        end
+    end
+    def sort
+        @roster.each {|key, val| val.sort!}
+    end
+end
